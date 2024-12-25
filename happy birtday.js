12fa2bb2fa0e31776622259cb634a07683148cec
ZW <f44966022@gs.ncku.edu.tw>
2024-12-25 11:56:50 +0800

    update

--- happy birtday.js
+++ happy birtday.js
@@ -5,7 +5,7 @@ let w = (c.width = window.innerWidth),
 (hh = h / 2),
   (opts = {
     // change the text in here //
-    strings: ["HAPPY", "BIRTHDAY!", "to Su"],
+    strings: ["HAPPY", "BIRTHDAY!", "to Suzy"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
